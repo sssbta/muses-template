17528fc3327131eb5b18091c7856ca19d031053f
mwu2227789 <166771755+mwu2227789@users.noreply.github.com>
2024-05-31 11:52:49 +0900

    修整

--- vue-template/cereer.js
+++ vue-template/cereer.js
@@ -31,35 +31,6 @@ PetiteVue.createApp({
     const endDayCount = endDate.getDate(); // 月の末日
     const startDay = startDate.getDay(); // 月の最初の日の曜日を取得
     let dayCount = 1; // 日にちのカウント
-    let calendarHtml = ""; // HTMLを組み立てる変数
-
-    calendarHtml += "<h1>" + year + "/" + month + "</h1>";
-    calendarHtml += "<table>";
-
-    // 曜日の行を作成
-    for (let i = 0; i < weeks.length; i++) {
-      calendarHtml += "<td>" + weeks[i] + "</td>";
-    }
-
-    for (let w = 0; w < 6; w++) {
-      calendarHtml += "<tr>";
-
-      for (let d = 0; d < 7; d++) {
-        if (w == 0 && d < startDay) {
-          // 1行目で1日の曜日の前
-          calendarHtml += "<td></td>";
-        } else if (dayCount > endDayCount) {
-          // 末尾の日数を超えた
-          calendarHtml += "<td></td>";
-        } else {
-          calendarHtml += "<td>" + dayCount + "</td>";
-          dayCount++;
-        }
-      }
-      calendarHtml += "</tr>";
-    }
-    calendarHtml += "</table>";
-
-    document.querySelector("#calendar").innerHTML = calendarHtml;
+    
   },
 }).mount();

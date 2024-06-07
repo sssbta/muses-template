1d6edcb833d44e1ce2fa091735cc7550b96cb09d
mwu2227789 <166771755+mwu2227789@users.noreply.github.com>
2024-06-07 11:25:21 +0900

    カレンダー

--- vue-template/cereer.js
+++ vue-template/cereer.js
@@ -31,9 +31,9 @@ PetiteVue.createApp({
     console.log(today);
     const year = today.getFullYear();
     this.year = year;
-    const month = today.getMonth() ;
+    const month = today.getMonth()+1 ;
     this.month = month ;
-    console.log(this.year, this.month);
+    console.log(this.year, this.month,month);
     const startDate = new Date(year, month - 1, 1); // 月の最初の日を取得
     const endDate = new Date(year, month, 0); // 月の最後の日を取得
     const endDayCount = endDate.getDate(); // 月の末日

3d5a80260bff52fa7f127390b9ea79eb6ea517a5
mwu2227789 <166771755+mwu2227789@users.noreply.github.com>
2024-07-05 10:54:12 +0900

    a

--- vue-template/cereer.js
+++ vue-template/cereer.js
@@ -92,9 +92,10 @@ PetiteVue.createApp({
       if (cell) {
         cell.innerHTML += `<div>${text}</div>`;
       }
+      console.log(cell);
     }
     createCalendar(year, month - 1);
-    console.log(cell);
+    
     insertText(year, month, 15, "イベント");
     console.log(this.text);
   },

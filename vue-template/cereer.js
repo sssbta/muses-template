6aeba851f53647553328ab3c1a2e1c0879c187bd
mwu2227789 <166771755+mwu2227789@users.noreply.github.com>
2024-07-05 11:31:07 +0900

    s

--- vue-template/cereer.js
+++ vue-template/cereer.js
@@ -86,7 +86,7 @@ PetiteVue.createApp({
           }else if(dayCount <= endDayCount){
             html += `<td id="day-${year}-${month}-${dayCount}">${dayCount}</td>`;
             dayCount++;
-          } else {
+          } else if(dayCount >= endDayCount) {
             html += "<td></td>";
           }
           // console.log(dayCount);
